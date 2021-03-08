51bbceb80fe4808cf5bc8f86fb0647526ad1220e
Richard Pérez (Aztec iMac) <rperez1972@gmail.com>
2021-03-08 13:06:40 -0600

    zxc

--- flower-drawing.py
+++ flower-drawing.py
@@ -18,9 +18,9 @@ babbage.end_fill()
 #draw all petal
 for i in range (1,24):
     if babbage.color() == ("red", "black"):
-        babbage.color("orange", "black")
-    elif babbage.color() == ("orange", "black"):
-        babbage.color("yellow", "black")
+        babbage.color("blue", "black")
+    elif babbage.color() == ("blue", "black"):
+        babbage.color("purple", "black")
     else:
         babbage.color("red", "black")
     babbage.left(15)
